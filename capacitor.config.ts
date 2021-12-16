c1ae272e34fe910a71b1d31d618b5dff7a53e46f
Mohammad Aakash <muhammadaakash249@gmail.com>
2021-12-16 13:32:38 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'foodpanda_clone',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
