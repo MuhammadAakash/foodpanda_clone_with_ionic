7a9d8ef5a7c6e5f70e940c5ea352150db71b2ec0
Mohammad Aakash <muhammadaakash249@gmail.com>
2021-12-28 17:52:36 +0500

    work with camera plugin and implement it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  imageProperties = {
+    quality:100,
+    allowEditing: false,
+    resultType: CameraResultType.DataUrl
+  }
+  theActualPicture = ""
   constructor() {}
+  
+
+  async takePicture(){
+    const image = await Camera.getPhoto(this.imageProperties)
+
+    this.theActualPicture = image.dataUrl
+
+  }
+  deleteimage(){
+    this.theActualPicture = ""
+  }
 }
